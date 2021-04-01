28f504a14e9baa8390f6b1b08d77d93999d70749
xieguanhua <359488454@qq.com>
2021-04-01 18:30:30 +0800

    xmind-to-json

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,9 @@
 const xmindParser = require('../');
 const path = require('path')
 let parser = new xmindParser()
-parser.xmindToJSON(path.join(__dirname,'./../test.xmind'))
-
+parser.xmindToJSON(path.join(__dirname,'./../test1.xmind')).then(v=>{
+    // console.log(JSON.stringify(v))
+})
 
 
 // parser.xmindToJSON('http://beta.file.neobai.com/previews/1377465659176390656.xmind')
