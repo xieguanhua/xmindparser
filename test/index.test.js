2ce54c283111d927eea5f2658b727bed2b074dbf
xieguanhua <359488454@qq.com>
2021-04-02 16:46:48 +0800

    单元测试

--- test/index.test.js
+++ test/index.test.js
@@ -6,36 +6,26 @@ const testFilePath = path.join(__dirname, 'tmp/test.xmind')
 
 let parser = new xmindParser()
 
-describe('xmind转json的本地路径测试', function () {
+const xmindToJSON=(data)=>{
     it('异步返回kityminder（脑图）的json格式', (done) => {
-        parser.xmindToJSON(testFilePath).then(res => {
+        parser.xmindToJSON(data).then(res => {
             expect(res).to.be.an('object');
             done();
         }).catch(e => {
             done(e);
         })
     })
+}
+
+describe('xmind转json的本地路径测试', function () {
+    xmindToJSON(testFilePath)
 });
 describe('xmind转json的网络路径测试', function () {
-    it('异步返回kityminder（脑图）的json格式', (done) => {
-        parser.xmindToJSON('http://beta.file.neobai.com/previews/1377465659176390656.xmind').then(res => {
-            expect(res).to.be.an('object');
-            done();
-        }).catch(e => {
-            done(e);
-        })
-    })
+    xmindToJSON('http://beta.file.neobai.com/previews/1377465659176390656.xmind')
 });
 describe('xmind转json的 二进制文件', function () {
     const file = fs.readFileSync(testFilePath)
-    it('异步返回kityminder（脑图）的json格式', (done) => {
-        parser.xmindToJSON(file).then(res => {
-            expect(res).to.be.an('object');
-            done();
-        }).catch(e => {
-            done(e);
-        })
-    })
+    xmindToJSON(file)
 });
 
 
